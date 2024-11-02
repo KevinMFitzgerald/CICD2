047fa55a6a66952760eb259acdef05fb90d0af5b
Kevcollector <kfitz2003@gmail.com>
2024-11-02 13:46:46 +0000

    Completed

--- src/main/java/ie/atu/week2cdci/RequestController.java
+++ src/main/java/ie/atu/week2cdci/RequestController.java
@@ -16,4 +16,35 @@ public class RequestController {
     {
         return "Your name is " + name +" and you are " + age + " years old ";
     }
+    @GetMapping("/calculate")
+    public String calculate(@RequestParam Float num1,@RequestParam Float num2,@RequestParam String operation)
+    {
+        float ans;
+        switch (operation)
+        {
+            case"add":
+                ans = num1 + num2;
+
+                break;
+            case"subtract":
+                ans = num1 - num2;
+                break;
+            case"divide":
+                if(num2==0)
+                {
+                    return "You cannot divide by zero";
+
+                }
+                else{
+                    ans = (float)num1/num2;
+                }
+                break;
+            case "multiply":
+                ans = num1*num2;
+                break;
+            default:
+                return "Please check the operator. Cap Sensitive";
+        }
+        return "The correct answer is: " + ans;
+    }
 }
