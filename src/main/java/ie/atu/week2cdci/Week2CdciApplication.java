772accb550bd0262deccd588c64ca8badbccf836
Kevcollector <kfitz2003@gmail.com>
2024-11-02 13:10:31 +0000

    First commit

---
+++ src/main/java/ie/atu/week2cdci/Week2CdciApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week2cdci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week2CdciApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week2CdciApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/week2cdci/Week2CdciApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week2cdci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week2CdciApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
