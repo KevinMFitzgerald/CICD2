78063d256936a15f83cefc4ede46142029d25b29
Kevcollector <kfitz2003@gmail.com>
2024-11-02 13:26:35 +0000

    Step 2-7

--- src/main/java/ie/atu/week2cdci/RequestController.java
+++ src/main/java/ie/atu/week2cdci/RequestController.java
@@ -2,8 +2,18 @@ package ie.atu.week2cdci;
 import org.springframework.web.bind.annotation.*;
 @RestController
 public class RequestController {
-    @GetMapping("/Hello")
+    @GetMapping("/hello")
     public String sayHello() {
         return "Hello there!";
+    }
+    @GetMapping("/greet/{name}")
+    public String greetByName(@PathVariable String name)
+    {
+        return "Your name is " +name;
+    }
+    @GetMapping ("/details")
+    public String details (@RequestParam String name, @RequestParam int age)
+    {
+        return "Your name is " + name +" and you are " + age + " years old ";
     }
 }
